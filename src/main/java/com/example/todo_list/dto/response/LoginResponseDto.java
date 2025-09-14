f805872d5fc96341a9e96e08b7baf9d66b53adb3
GPT와 함께라면 두렵지 않아 <isjuny0k@gmail.com>
2025-09-14 21:51:32 +0900

    feat:Create signup and login method and fix etc.

--- src/main/java/com/example/todo_list/dto/response/LoginResponseDto.java
+++ src/main/java/com/example/todo_list/dto/response/LoginResponseDto.java
@@ -1,5 +1,10 @@
 package com.example.todo_list.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class LoginResponseDto {
 
     private String token;
